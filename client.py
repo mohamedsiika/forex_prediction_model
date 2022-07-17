b76ad65b8ca12d4f2ec34c6bd71ecb87bb767852
Loai Gamal <loai.gamal@gmail.com>
2022-07-18 01:30:48 +0200

    insha'allah 5eer

--- client.py
+++ client.py
@@ -1,110 +1,188 @@
-import pickle
-import socket
+import sys
 import MetaTrader5 as mt5
-from datetime import datetime, timedelta
-import pytz
-import time
 import pandas as pd
-from MA_action_funs import *
-from DataPreprocessing_funs import *
-import matplotlib.pyplot as plt
+import requests
+import numpy as np
+from streamlit import cli as stlci
+import schedule
+import time
+import threading
+import os
+from dashboard import*
+
+
 
 def initialize_meta():
     if not mt5.initialize():
-        print("initialize() failed, error code =", mt5.last_error())
+        print("initialize() failed, error code = ", mt5.last_error())
         quit()
 
 
 def get_data():
-    # timezone = pytz.timezone('ETC/UTC')
-    # now = datetime.now()
-    # from_date = datetime.now() - timedelta(hours=78)
-    # print(now)
-    # print(from_date)
-
-    df = pd.DataFrame(mt5.copy_rates_from_pos('EURUSD', mt5.TIMEFRAME_H1, 22, 78))
+    global positions, profit
+    df = pd.DataFrame(mt5.copy_rates_from_pos('EURUSD', mt5.TIMEFRAME_H1, 0, 141))
     df['time'] = pd.to_datetime(df['time'], unit='s')
+    try:
+        client_info = pd.read_csv('client_info.csv')
+        flag = 1
+        header = False
+    except:
+        flag = 0
+        positions = np.array([[2] for i in range(141)])
+        positions = np.array(positions).reshape(141, 1)
 
-    return df
+        profit = np.array([[0] for i in range(141)])
+        profit = np.array(profit).reshape(141, 1)
+
+        header = True
+
+    if flag == 1:
+        temp = pd.DataFrame(mt5.copy_rates_from_pos('EURUSD', mt5.TIMEFRAME_H1, 0, 1))
+        temp = temp.values
+        position_opened = mt5.positions_get()
+        if len(position_opened) == 0:
+            current_pos = 2
+            current_profit = 0
+        else:
+            current_pos = position_opened[0].type
+            current_profit = position_opened[0].profit
+
+        data = np.concatenate((temp, np.array([[current_pos]]), np.array([[current_profit]])), axis=1)
+
+    else:
+        # print(df.values.shape, positions.shape, profit.shape)
+        data = np.concatenate((df.values, positions, profit), axis=1)
+
+    dataframe = pd.DataFrame(data=data, columns=['time', 'open', 'high', 'low', 'close', 'tick_volume', 'spread',
+                                                 'real_volume', 'positions', 'profit'])
+
+    dataframe.to_csv('client_info.csv', index=False, mode='a', header=header)
 
 
-def account_login(accountID):
-    authorized = mt5.login(login=accountID, password="lwnljii8", server="MetaQuotes-Demo")
+def account_login(accountID=59238033, password="dezjrca7"):
+    global id, passw
+    id = accountID
+    passw = password
+    authorized = mt5.login(login=accountID, password=password, server="MetaQuotes-Demo")
     if authorized:
-        # display trading account data 'as is'
-        print(mt5.account_info())
-        # display trading account data in the form of a list
-        print("Show account_info()._asdict():")
-        account_info_dict = mt5.account_info()._asdict()
-
+        # print(mt5.account_info())
+        # print("Show account_info()._asdict():")
         return authorized
-
     else:
         print("failed to connect at account #{}, error code: {}".format(account, mt5.last_error()))
         return None
 
 
-account_id = 5003644199
 initialize_meta()
-df = get_data()
-account = account_login(account_id)
-
-print(df)
+account = account_login()
 
 
-FORMAT = "utf-8"
-HEADER = 64
-DISCONNECT_MSG = '!DISCONNECTED'
-port = 5050
-IP = socket.gethostbyname(socket.gethostname())  # gets the ip server
-ADDR = (IP, port)
+def updating_models_data(req):
+    data = []
+    for key in req.keys():
+        print(key)
+        if req[key] != 0:
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(ADDR)
+            if key == 'H1':
+                df = pd.DataFrame(mt5.copy_rates_from_pos('EURUSD', mt5.TIMEFRAME_H1, 0, req[key]))
+                df['time'] = pd.to_datetime(df['time'], unit='s')
+                return df
+    return data
 
 
-def send_msg(msg):
-    message = msg
-    client.send(message)
-    msg=client.recv(1000)
-    while msg:
-        print('Received:' + msg.decode())
-        trade,sl,tp=Action(1.06993,msg)
-        print(trade)
-        if trade!="no_action":
-            if trade=="buy":
-                trade_type=mt5.ORDER_TYPE_BUY
-            else:
-                trade_type=mt5.ORDER_TYPE_SELL
+def trade():
+    df = pd.read_csv('client_info.csv')
+    df = df.iloc[-141:]
+    dataset = df.to_dict(orient='list')
+    post_data = {'df': dataset}
+    # post_data_url = "https://forex-app-fudn2.ondigitalocean.app/getAction"
+    post_data_url = "http://127.0.0.1:5000/getAction"
+    r_action = requests.post(post_data_url, json=post_data, stream=True)
+    response_action = r_action.json()
+    action = response_action['Response']['Action']
 
+    if action == "0":
+        if len(mt5.positions_get()) == 0:
+            trade_type = mt5.ORDER_TYPE_BUY
             buy_request = {
                 "action": mt5.TRADE_ACTION_DEAL,
                 "symbol": "EURUSD",
-                "volume": 0.1,
+                "volume": 0.01,
                 "type": trade_type,
-                "price": 1.06993,
-                "sl": sl,
-                "tp": tp,
-                "comment": "sent by python",
-
+                "comment": "sent by Barter",
             }
+            print("BUY")
 
-            result = mt5.order_send(buy_request)
-            print(result)
-            print(sl,tp)
-
-
-
-
-
-        msg = client.recv(1024)
-
-
-
-
-
+        elif mt5.positions_get()[0].type == 1:
+            trade_type = mt5.ORDER_TYPE_BUY
+            buy_request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": "EURUSD",
+                "volume": 0.01,
+                "type": trade_type,
+                "comment": "sent by Barter",
+                "position": mt5.positions_get()[0].ticket
+            }
+            print("SELL CLOSE")
 
 
 
 
-send_msg(pickle.dumps(df,protocol=4))
+    elif action == "1":
+        if len(mt5.positions_get()) == 0:
+            trade_type = mt5.ORDER_TYPE_SELL
+            buy_request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": "EURUSD",
+                "volume": 0.01,
+                "type": trade_type,
+                "comment": "sent by Barter",
+            }
+            print("BUY")
+
+        elif mt5.positions_get()[0].type == 0:
+            trade_type = mt5.ORDER_TYPE_SELL
+            buy_request = {
+                "action": mt5.TRADE_ACTION_DEAL,
+                "symbol": "EURUSD",
+                "volume": 0.01,
+                "type": trade_type,
+                "comment": "sent by Barter",
+                "position": mt5.positions_get()[0].ticket
+            }
+            print("BUY CLOSE")
+
+    else:
+        print("HOLD")
+
+    # get_update_need_url = "https://forex-app-fudn2.ondigitalocean.app/dataUpdate"
+    get_update_need_url = "http://127.0.0.1:5000/dataUpdate"
+    r_updates = requests.get(get_update_need_url)
+    response_updates = r_updates.json()
+    print(response_updates['Response']['Dict'])
+    if response_updates['Response']['Dict'] != "False":
+        updated_data = updating_models_data(dict(response_updates['Response']['Dict']))
+        # print("update data", '\n', updated_data)
+        updated_data['time'] = updated_data['time'].astype(str)
+
+        dataset = updated_data.to_dict(orient='list')
+        post_data = {'df': dataset}
+        # update_models_url = "https://forex-app-fudn2.ondigitalocean.app/updateModels"
+        update_models_url = "http://127.0.0.1:5000/updateModels"
+        r_update_models = requests.post(update_models_url, json=post_data)
+        response_update_models = r_update_models.json()
+        # print(response_update_models)
+
+
+def trade_every_hour():
+    schedule.every(1).hours.do(trade)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == '__main__':
+    trade()
+    thread = threading.Thread(target=trade_every_hour)
+    thread.start()
+    os.system('cmd /c "streamlit run "E:/Study/University/FourthYear/Second Term/GP/Client/dashboard.py""')
+
--- dashboard.py
+++ dashboard.py
@@ -7,111 +7,100 @@ import streamlit as st
 
 import numpy as np
 from streamlit_option_menu import option_menu
+from streamlit import cli as stlci
+import sys
+import re
 
 
-# display data on the MetaTrader 5 package
-print("MetaTrader5 package author: ", mt5.__author__)
-print("MetaTrader5 package version: ", mt5.__version__)
-
-# establish MetaTrader 5 connection to a specified trading account
-if not mt5.initialize():
-    print("initialize() failed, error code =", mt5.last_error())
-    quit()
-
-account_info=mt5.account_info()
-balance=account_info.balance
-login=account_info.login
-equity=account_info.equity
-print(balance)
+def initialize_meta():
+    if not mt5.initialize():
+        print("initialize() failed, error code = ", mt5.last_error())
+        quit()
 
 
-# get the number of deals in history
-from_date=datetime(2020,1,1)
-to_date=datetime.now()
+initialize_meta()
 
-deals=mt5.history_deals_get(from_date, to_date)
-deals=list(deals)
-for i in deals:
-    if i.profit==0:
-        deals.remove(i)
-
-deals_number=len(deals)
-
-num_current_pos=mt5.positions_total()
-pos = mt5.positions_get()
-# for open cuurent orders
-
-positions=pd.DataFrame(list(pos),columns=pos[0]._asdict().keys())
-positions['time'] = pd.to_datetime(positions['time'], unit='s')
-opendf = positions.loc[:, ['profit']]
-opendf = opendf.iloc[0: , :]
-
-positions=positions.drop(columns=['ticket','time_msc','time_update_msc','time_update','sl','tp','swap','external_id','magic'])
-
-# for trade history deals
-
-deals=pd.DataFrame(list(deals),columns=deals[0]._asdict().keys())
-deals['time'] = pd.to_datetime(deals['time'], unit='s')
-df = deals.loc[:, ['profit']]
-df = df.iloc[1: , :]
-deals = deals.iloc[1: , :]
-
-print(df)
-
-deals=deals.drop(columns=['ticket','swap','external_id','magic','commission','fee','reason','position_id','order'])
+account_info = mt5.account_info()
+balance = account_info.balance
+login = account_info.login
+equity = account_info.equity
 
 
+def main():
+    # get the number of deals in history
+    from_date = datetime(2020, 1, 1)
+    to_date = datetime.now()
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",  # required
-        options=["Home", "Tarde History Deals", "Current Deals"], # required
-        icons=["house", "bar-chart-line", "activity"],  # optional
-        menu_icon="cast",  # optional
-        default_index=0,  # optional
-                )
+    deals = mt5.history_deals_get(from_date, to_date)
+    deals = list(deals)
+    for i in deals:
+        if i.profit == 0:
+            deals.remove(i)
+
+    deals_number = len(deals)
+
+    num_current_pos = mt5.positions_total()
+    pos = mt5.positions_get()
+    # for open cuurent orders
+
+    if len(pos) > 0:
+        positions = pd.DataFrame(list(pos), columns=pos[0]._asdict().keys())
+        positions['time'] = pd.to_datetime(positions['time'], unit='s')
+        opendf = positions.loc[:, ['profit']]
+        opendf = opendf.iloc[0:, :]
+
+        positions = positions.drop(
+            columns=['ticket', 'time_msc', 'time_update_msc', 'time_update', 'sl', 'tp', 'swap', 'external_id',
+                     'magic'])
+
+    # for trade history deals
+
+    deals = pd.DataFrame(list(deals), columns=deals[0]._asdict().keys())
+    deals['time'] = pd.to_datetime(deals['time'], unit='s')
+    df = deals.loc[:, ['profit']]
+    df = df.iloc[1:, :]
+    deals = deals.iloc[1:, :]
+
+    deals = deals.drop(
+        columns=['ticket', 'swap', 'external_id', 'magic', 'commission', 'fee', 'reason', 'position_id', 'order'])
+
+    with st.sidebar:
+        selected = option_menu(
+            menu_title="Main Menu",  # required
+            options=["Home", "Tarde History Deals", "Current Deals"],  # required
+            icons=["house", "bar-chart-line", "activity"],  # optional
+            menu_icon="cast",  # optional
+            default_index=0,  # optional
+        )
+
+    show_positions = st.sidebar.checkbox(label="show current positions")
+    if show_positions:
+        st.title("Positions")
+        st.write(positions)
+
+    show_deals = st.sidebar.checkbox(label="show deals history")
+    if show_deals:
+        st.title("history")
+        st.write(deals)
+
+    if selected == "Home":
+        st.title(f" {selected}")
+        st.write(f'This is your  Login number {login}')
+        st.write(f'This is your  Current balance {balance}')
+        st.write(f'This is your  Current Equity {equity}')
+    if selected == "Tarde History Deals":
+        st.title(f" {selected}")
+        st.write(f'This is your Total number of deals {deals_number}')
+        # st.bar_chart(df3)
+        st.line_chart(df)
+
+    if selected == "Current Deals":
+        st.title(f" {selected}")
+        st.write(f'Number of current open trades {num_current_pos}')
+        if len(pos) > 0:
+            plot2 = px.bar(data_frame=opendf, x=opendf.index, y=opendf['profit'], width=500)
+            st.plotly_chart(plot2)
 
 
-show_positions=st.sidebar.checkbox(label="show current positions")
-if show_positions:
-    st.title("Positions")
-    st.write(positions)
-
-show_deals=st.sidebar.checkbox(label="show deals history")
-if show_deals:
-    st.title("history")
-    st.write(deals)
-
-
-if selected == "Home":
-    st.title(f" {selected}")
-    st.write(f'This is your  Login number {login}')
-    st.write(f'This is your  Current balance {balance}')
-    st.write(f'This is your  Current Equity {equity}')
-if selected == "Tarde History Deals":
-    st.title(f" {selected}")
-    st.write(f'This is your Total number of deals {deals_number}')
-    # st.bar_chart(df3)
-    st.line_chart(df)
-
-
-if selected == "Current Deals":
-    st.title(f" {selected}")
-    st.write(f'Number of current open trades {num_current_pos}')
-    plot2 = px.bar(data_frame=opendf, x=opendf.index, y=opendf['profit'],width=500)
-    st.plotly_chart(plot2)
-
-    #st.line_chart(opendf)
-    # if pos  ==None:
-    #     print("No positions ".format(mt5.last_error()))
-    #     st.write(f'Number of current open order {num_current_order}')
-    #     st.write(f'Number of current open trades {num_current_pos}')
-    #
-    # elif len(pos) > 0:
-    #     print("positions_get".format(len(pos)))
-    #     # display these positions as a table using pandas.DataFrame
-    #     opendf = pd.DataFrame(list(pos), columns=pos[0]._asdict().keys())
-    #     opendf['time'] = pd.to_datetime(opendf['time'], unit='s')
-    #     opendf = opendf.loc[:, ['profit']]
-    #     opendf = opendf.iloc[1:, :]
-    #     st.line_chart(opendf)
+if __name__ == '__main__':
+    main()
